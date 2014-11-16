f1c59ce750603a3b088e0c70f274040c17f0a378
Nathan Owsiany <nowsiany@gmail.com>
2014-11-15 18:45:31 -0700

    fake cleaners in Entry

--- lib/event_reporter/entry.rb
+++ lib/event_reporter/entry.rb
@@ -5,14 +5,18 @@ module EventReporter
     def initialize(data)
       @id = data[:_]
       @reg_date = data[:regdate]
-      @first_name = data[:first_name]
-      @last_name = data[:last_name]
+      @first_name = name_cleaner(data[:first_name])
+      @last_name = name_cleaner(data[:last_name])
       @email = data[:email]
       @phone = data[:homephone]
       @street = data[:street]
       @city = data[:city]
       @state = data[:state]
       @zipcode = data[:zipcode]
+    end
+
+    def name_cleaner(name)
+      name
     end
   end
 end
