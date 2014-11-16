3789aa9009937bd7bd7f95de4dac766780dce704
Nathan Owsiany <nowsiany@gmail.com>
2014-11-15 23:44:21 -0700

    cleanup load fomatting, add load messages

--- lib/event_reporter/load.rb
+++ lib/event_reporter/load.rb
@@ -2,26 +2,28 @@ require 'csv'
 
 module EventReporter
   class Load
-    attr_reader :printer
-    attr_accessor :file_name, :criteria
+    attr_reader   :printer
+    attr_accessor :file_name,
+                  :criteria
 
     def initialize(instream, outstream, printer, criteria)
-      @instream = instream
-      @outstream = outstream
-      @printer = printer
-      @criteria = criteria
-      @file_name = ''
+      @instream   = instream
+      @outstream  = outstream
+      @printer    = printer
+      @criteria   = criteria
+      @file_name  = ''
     end
 
     def call
       get_filename
-      load_file
+      file_exists? ? load_file : printer.invalid_file_name(file_name)
+      printer.confirm_file_load(@@entry_repository.entries.length)
     end
 
     def load_file
-      generate_file_path
-      contents = read_in_csv
-      csv_rows = contents.map { |row| row.to_hash }
+      file_path          = generate_file_path
+      contents           = read_in_csv(file_path)
+      csv_rows           = contents.map { |row| row.to_hash }
       @@entry_repository = EventReporter::EntryRepository.new(csv_rows)
     end
 
@@ -36,7 +38,11 @@ module EventReporter
       File.join(EventReporter::LOAD_FILE_DIR, file_name)
     end
 
-    def read_in_csv
+    def file_exists?
+      File.exist?(generate_file_path)
+    end
+
+    def read_in_csv(file_path)
       CSV.open file_path, headers: true, header_converters: :symbol
     end
 
--- lib/event_reporter/printer.rb
+++ lib/event_reporter/printer.rb
@@ -25,5 +25,13 @@ module EventReporter
     def invalid_load_criteria(criteria_length)
       outstream.puts "Invalid load criteria: you entered #{criteria_length} criterion, should have been 1 or 0."
     end
+
+    def invalid_file_name(file_name)
+      outstream.puts "'#{file_name}' does not exist in the projects 'files' directory"
+    end
+
+    def confirm_file_load(entries_loaded)
+      outstream.puts ">>> Load Complete: #{entries_loaded} entries added to the Entry Repository"
+    end
   end
 end
